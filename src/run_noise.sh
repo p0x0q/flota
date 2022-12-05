b219baaa32a597608f97d5fe776e9165e4660d31
p0x0q <info.allegation@gmail.com>
2022-12-05 23:50:23 +0900

    python3.8 to python3

--- src/run_noise.sh
+++ src/run_noise.sh
@@ -2,7 +2,7 @@
 
 for data in arxiv_cs_1e+02 arxiv_maths_1e+02 arxiv_physics_1e+02 arxiv_cs_1e+03 arxiv_maths_1e+03 arxiv_physics_1e+03
 do
-	python3.8 -u main.py \
+	python3 -u main.py \
 	--batch_size 64 \
 	--n_epochs 20 \
 	--data $data \
@@ -17,7 +17,7 @@ for k in 1 2 3 4
 do 
 	for data in arxiv_cs_1e+02 arxiv_maths_1e+02 arxiv_physics_1e+02 arxiv_cs_1e+03 arxiv_maths_1e+03 arxiv_physics_1e+03
 	do
-		python3.8 -u main.py \
+		python3 -u main.py \
 		--batch_size 64 \
 		--n_epochs 20 \
 		--data $data \
